91317b1e3bab3ca724e8c55c8cb20305971f8f9b
VHEC-Quynh <le.nguyen.xuan.quynh@vhec.vn>
2021-10-12 17:28:43 +0700

    cm

---
+++ mysite/polls/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+ 
+# Register your models here.
+from .models import Question, Choice
+ 
+# class ChoiceInLine(admin.StackedInline):
+class ChoiceInLine(admin.TabularInline):
+    model = Choice 
+    extra = 3
+  
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInLine]
+ 
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
